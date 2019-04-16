98aba62ce338bda801840d0e464a50d0a6e12ed4
Linus Blom <linus.blom@me.com>
2019-04-16 20:22:33 +0200

    update not found page

--- src/app/core/components/not-found/not-found.component.ts
+++ src/app/core/components/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faSadTear } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  icon = faSadTear;
+}

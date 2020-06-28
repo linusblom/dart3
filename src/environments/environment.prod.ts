2ce8b3556e723f83b5a1ed3a25a273afcc9efb0a
Linus Blom <linus.blom@me.com>
2020-06-28 23:18:55 +0200

    update envs and add logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,12 @@ import { version } from '../../package.json';
 
 export const environment = {
   production: true,
-  local: false,
-  siteUrl: '',
-  dart3ApiUrl: '',
+  siteUrl: 'https://dart3.app',
+  dart3ApiUrl: 'http://api.dart3.app',
   version,
   auth0: {
     domain: 'dart3.eu.auth0.com',
     clientId: '9wZ3rNbCqBoKuCfLK44ADhLCsGzebs4x',
-    audience: 'https://stage.dart3.linusblom.io',
+    audience: 'https://dart3.app',
   },
 };
